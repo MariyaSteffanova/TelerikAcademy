588dc601438393a7a835f490f1a5ad4001e20e02
MariyaSteffanova <mariya.steffanova@gmail.com>
2015-03-18 13:40:42 +0200

    upload homework

---
+++ Homeworks/OOP/Extension-Methods-Delegates-Lambda-LINQ/StringBuilder.Substring/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuilderSubstring
+{
+   public static class Extensions
+    {
+        public static StringBuilder Substring(this StringBuilder sb, int index, int lenght)
+        {
+            var newSb = new StringBuilder();
+
+            for (int i = 0 ; i < lenght ; i++)
+            {
+                newSb.Append(sb[i+index]);
+            }
+
+            return newSb;        
+        }
+    }
+}
