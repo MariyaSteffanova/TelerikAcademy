2c1ce104a33d2ff887b3307c4adae51159b1bbd4
MariyaSteffanova <mariya.steffanova@gmail.com>
2015-03-18 13:33:49 +0200

    upload homework
    .

---
+++ Homeworks/OOP/Extension-Methods-Delegates-Lambda-LINQ/Students/Students.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Students
+{
+    class Student
+    {
+        public Student(string fName, string lName, int age)             
+        {
+            this.FirstName = fName;
+            this.LastName = lName;
+            this.Age = age;
+        }
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append(this.FirstName + " ");
+            sb.Append(this.LastName);
+            return sb.ToString();
+        }
+    }
+}
