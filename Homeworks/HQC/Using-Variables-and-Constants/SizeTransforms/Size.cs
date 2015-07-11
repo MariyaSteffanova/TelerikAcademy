390673e11e8a61e9b88569074b9d71336dbf74d4
MariyaSteffanova <mariya.steffanova@gmail.com>
2015-07-11 14:56:54 +0300

    HQC [Using-Variables-and-Constants] HW

---
+++ Homeworks/HQC/Using-Variables-and-Constants/SizeTransforms/Size.cs
@@ -0,0 +1,35 @@
+﻿namespace SizeTransforms
+{
+    using System;
+
+    public class Size
+    {
+        public Size(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Width { get; set; }
+
+        public double Height { get; set; }
+
+        public Size GetRotatedSize(double angleOfRotation)
+        {            
+            double angleCos = Math.Abs(Math.Cos(angleOfRotation));
+            double angleSin = Math.Abs(Math.Sin(angleOfRotation));
+
+            double newWidth = (angleCos * this.Width) + (angleSin * this.Height);
+            double newHeight = (angleSin * this.Width) + (angleCos * this.Height);
+
+            Size rotatedSize = new Size(newWidth, newHeight);
+
+            return rotatedSize;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Width: {0:F2}, Height: {1:F2}", this.Width, this.Height);
+        }
+    }
+}
---
+++ Homeworks/HQC/Using-Variables-and-Constants/SizeTransforms/SizeTransforms.cs
@@ -0,0 +1,16 @@
+﻿namespace SizeTransforms
+{
+    using System;
+
+    public class SizeTransforms
+    {
+        public static void Main()
+        {
+            var size = new Size(10.0, 20.0);
+            var rotatedSize = size.GetRotatedSize(90);
+
+            Console.WriteLine(size);
+            Console.WriteLine(rotatedSize);
+        }
+    }
+}
