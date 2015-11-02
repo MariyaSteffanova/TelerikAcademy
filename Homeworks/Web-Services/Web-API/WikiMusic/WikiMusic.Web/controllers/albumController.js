30e6706f2c1299a0cda9fdd043930a3431e21d9b
MariyaSteffanova <mariya.steffanova@gmail.com>
2015-11-02 13:15:36 +0200

    changes on albums client side

--- Homeworks/Web-Services/Web-API/WikiMusic/WikiMusic.Web/controllers/albumController.js
+++ Homeworks/Web-Services/Web-API/WikiMusic/WikiMusic.Web/controllers/albumController.js
@@ -10,8 +10,22 @@
             })
             .then(function(partial) {
                 context.$element().html(partial(album));
+            })
+            .then(function() {
+                $('#wrpper-editable-options').on('click', 'button', function (e) {
+                    var $target = $(e.target)[0],
+                        target = e.target;
+
+                    if ($(target).hasClass('edit')) {
+                        edit($target, context);
+                    } else if ($(target).hasClass('delete')) {
+                        remove($target);
+                    }
+                });
             });
     }
+
+
 
     return {
         show: show
