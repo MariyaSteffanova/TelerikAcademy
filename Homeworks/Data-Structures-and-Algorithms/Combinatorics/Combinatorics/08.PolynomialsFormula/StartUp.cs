5b5d46455cc8705b1c27dd25d189868886394171
MariyaSteffanova <mariya.steffanova@gmail.com>
2015-11-14 01:00:02 +0200

    [DSA] Polynomial Formula. Task 8 [Algo Oct 2012]

---
+++ Homeworks/Data-Structures-and-Algorithms/Combinatorics/Combinatorics/08.PolynomialsFormula/StartUp.cs
@@ -0,0 +1,59 @@
+﻿namespace PolynomialsFormula
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Numerics;
+    using System.Text;
+
+    class Program
+    {
+        static void Main()
+        {
+            var polyCoef = Console.ReadLine().ToArray();
+            char a = polyCoef[1];
+            char b = polyCoef[3];
+            string input;
+            do
+            {
+                input = Console.ReadLine().Trim();
+            }
+            while (string.IsNullOrEmpty(input));
+
+            int pow = int.Parse(input);
+            int n = pow + 1;
+
+            long[] pascalCoef = new long[n];
+
+            pascalCoef[0] = 1;
+
+            for (long y = 0; y < n; y++)
+            {
+                long c = 1;
+
+                for (long x = 0; x < y; x++)
+                {
+                    c = c * (y - x) / (x + 1);
+                    pascalCoef[x + 1] = c;
+                }
+
+            }
+
+          //   (c ^ 3) + 3(c ^ 2)(y ^ 1) + 3(c ^ 1)(y ^ 2) + (y ^ 3)
+            var sb = new StringBuilder();
+            sb.AppendFormat("({0}^{1})+", a, pow);
+            var currentPowА = pow;
+            var currentPowB = 0;
+            for (int i = 1; i < n - 1; i++)
+            {
+                var coef = pascalCoef[i];
+               
+                    sb.AppendFormat("{0}({1}^{2})({3}^{4})+", coef, a, --currentPowА, b, ++currentPowB);
+               
+            }
+            sb.AppendFormat("({0}^{1})", b, pow);
+
+            Console.WriteLine(sb);
+        }
+    }
+}
