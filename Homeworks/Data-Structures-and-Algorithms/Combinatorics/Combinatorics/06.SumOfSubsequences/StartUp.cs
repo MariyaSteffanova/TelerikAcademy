70763aaddc77f5a276a4b6b354e94215ff7fa565
MariyaSteffanova <mariya.steffanova@gmail.com>
2015-11-13 23:59:08 +0200

    [DSA]

---
+++ Homeworks/Data-Structures-and-Algorithms/Combinatorics/Combinatorics/06.SumOfSubsequences/StartUp.cs
@@ -0,0 +1,45 @@
+﻿namespace _06.SumOfSubsequences
+{
+    using System;
+    using System.Linq;
+    using System.Numerics;
+
+    class StartUp
+    {
+        static void Main()
+        {
+            var test = int.Parse(Console.ReadLine());
+
+            var factorials = new BigInteger[1001];
+
+            factorials[0] = 1;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                factorials[i + 1] = factorials[i] * (i + 1);
+            }
+
+            for (int i = 0; i < test; i++)
+            {
+                var nk = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var n = nk[0];
+                var k = nk[1];
+
+                int sum = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .Sum();
+
+                var coef = factorials[n - 1] / (factorials[n - k - 1] * factorials[k]);
+
+                
+                Console.WriteLine(coef * sum);
+
+            }
+        }
+    }
+}
