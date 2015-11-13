9251a9a63e090bba96c4eb77ffd8877868a2ea0e
MariyaSteffanova <mariya.steffanova@gmail.com>
2015-11-14 01:43:50 +0200

    [DSA] Unufri Balls. Task 4 [Algo Oct 2012]

---
+++ Homeworks/Data-Structures-and-Algorithms/Combinatorics/Combinatorics/04.UnufriBalls/StartUp.cs
@@ -0,0 +1,60 @@
+﻿namespace _04.UnufriBalls
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Numerics;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class StartUp
+    {
+        static void Main()
+        {
+            //RYYRYBY -> N = 7, K =3,
+            //[R] -> 2
+            //[Y] -> 4
+            //[B] -> 1
+            
+            var input = Console.ReadLine().Trim();
+            var balls = new Dictionary<char,int>();
+            int N = input.Length;
+            int K;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var ch = input[i];
+                if (!balls.ContainsKey(ch))
+                {
+                    balls.Add(ch, 0);
+                }
+
+                balls[ch]++;
+            }
+
+            var factorials = new BigInteger[N+2];
+
+            factorials[0] = 1;
+
+            for (int i = 0; i <= N; i++)
+            {
+                factorials[i + 1] = factorials[i] * (i + 1);
+            }
+
+           
+
+           
+            var permutations = factorials[N];
+
+           
+            foreach (var x in balls.Values)
+            {
+                permutations /= factorials[x];
+            }
+
+           // result *= nColors;
+            Console.WriteLine(permutations);
+
+        }
+    }
+}
