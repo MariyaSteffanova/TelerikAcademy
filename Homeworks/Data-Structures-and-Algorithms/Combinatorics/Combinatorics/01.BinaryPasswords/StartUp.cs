a94ca9f5f6ff737f029f40cf52f34c76344f8774
MariyaSteffanova <mariya.steffanova@gmail.com>
2015-11-14 02:56:44 +0200

    [DSA] Binary Passwords. Task 1 [Algo Oct 2012]

---
+++ Homeworks/Data-Structures-and-Algorithms/Combinatorics/Combinatorics/01.BinaryPasswords/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace _01.BinaryPasswords
+{
+    using System;
+    using System.Linq;
+
+    class StartUp
+    {
+        // 268435456   2 ^ 60
+        // 2147483647  int.max
+        static void Main()
+        {
+            int input = Console.ReadLine().Where(x => x == '*').Count();
+            Console.WriteLine((long)1 << input);
+           
+        }
+    }
+}
